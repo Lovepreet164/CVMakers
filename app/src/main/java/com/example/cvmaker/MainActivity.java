985ed8d735913f4e7fa897c1f595071d97a75dbd
Lovepreet Kaur <lovepreetkaur0233@gmail.com>
2020-07-15 14:55:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/cvmaker/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.cvmaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       button= findViewById(R.id.btn);
+
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+              openHomePage();
+
+           }
+       });
+
+
+    }
+
+    private void openHomePage() {
+        Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
+    }
+}
